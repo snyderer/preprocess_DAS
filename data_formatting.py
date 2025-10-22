it works! fixed the scaling issue from interpolation, and have sample-accurate F-K filter construction. Also fixed tapering issue.

data_formatting.py
@@ -9,6 +9,7 @@ import numpy as np
 from scipy.interpolate import RegularGridInterpolator
 from scipy import ndimage
 from scipy.signal import windows
+from scipy import signal as sp
 import das4whales as dw
 
 def fk_interpolate(data, dx, fs, new_dx, new_fs, output_format='fk'):
@@ -71,7 +72,7 @@ def fk_interpolate(data, dx, fs, new_dx, new_fs, output_format='fk'):
     
     # Calculate scaling factors to preserve amplitude
     # This accounts for the change in sampling intervals
-    scale_factor = (new_dx / dx) * (new_dt / dt)
+    scale_factor = (dx/new_dx) * (dt/new_dt)
     
     # Interpolate in f-k domain
     interp = RegularGridInterpolator((k, f), D, bounds_error=False, fill_value=0)
@@ -92,7 +93,7 @@ def fk_interpolate(data, dx, fs, new_dx, new_fs, output_format='fk'):
         raise ValueError('output_format must be "fk" or "tx"')
 
 def create_fk_mask(shape, dx, fs, cs_min=1300, cp_min=1460, cp_max=6000, cs_max=7000,  
-                   fmin=None, fmax=None, return_half = True, fft_shift = True):
+                   fmin=15, fmax=90, df_taper=14, return_half = True, fft_shift = True):
     """
     create an fk filter mask using das4whales
 
@@ -117,11 +118,26 @@ def create_fk_mask(shape, dx, fs, cs_min=1300, cp_min=1460, cp_max=6000, cs_max=
     #                                cs_min=cs_min, cp_min=cp_min,
     #                                cp_max=cp_max, cs_max=cs_max)
     nx, ns = shape
-    fk_mask = np.zeros(shape).astype(complex)
 
     freq = np.fft.fftfreq(shape[1], d=1/fs)
     knum = np.fft.fftfreq(shape[0], d=dx)
 
+    # design butterworth filter for taper edges:
+    b, a = sp.butter(8, [fmin/(fs/2), fmax/(fs/2)], 'bp')
+    _, h = sp.freqz(b, a, worN=freq, fs=fs)
+    H = np.abs(h)**2
+    
+    fk_mask = np.tile(H, (len(knum), 1))
+    
+    # Apply taper to the frequencies of interest:
+    fpmax = fmax + df_taper
+    fpmin = fmin - df_taper
+
+    # Find the corresponding indexes:
+    fmin_idx = np.argmax(freq >= fpmin)
+    fmax_idx = np.argmax(freq >= fpmax)
+
+    # TODO I think I can vectorize this loop:
     for i in range(len(knum)):
         fs_min = np.abs(knum[i] * cs_min)
         fp_min = np.abs(knum[i] * cp_min)
@@ -129,15 +145,43 @@ def create_fk_mask(shape, dx, fs, cs_min=1300, cp_min=1460, cp_max=6000, cs_max=
         fp_max = np.abs(knum[i] * cp_max)
         fs_max = np.abs(knum[i] * cs_max)
 
-        idx = np.where(np.logical_and(np.abs(freq)>=fmin, np.abs(freq)<=fmax))
-        fk_mask[i, idx] = 1
+        filter_line = np.ones(shape=[len(freq)], dtype=float, order='F')
 
-    if fft_shift:
-        fk_mask = np.fft.fftshift(fk_mask)
+        if fs_min != fp_min:
+            # Filter transition band, ramping up from cs_min to cp_min
+            selected_speed_mask = ((freq >= fs_min) & (freq <= fp_min)) # positive frequencies
+            filter_line[selected_speed_mask] = np.sin(0.5 * np.pi *
+                                                        (freq[selected_speed_mask] - fs_min) / (fp_min - fs_min))
+            
+            selected_speed_mask = ((-freq >= fs_min) & (-freq <= fp_min)) # negative frequencies
+            filter_line[selected_speed_mask] = np.sin(0.5 * np.pi *
+                                                        (freq[selected_speed_mask] + fs_min) / (fp_min - fs_min))
+            
+        if fs_max != fp_max:
+            # Filter transition band, going down from cp_max to cs_max
+            selected_speed_mask = ((freq >= fp_max) & (freq <= fs_max)) # positive frequencies
+            filter_line[selected_speed_mask] = np.cos(0.5 * np.pi *
+                                                            (freq[selected_speed_mask] - fp_max) / (fs_max - fp_max))
+            
+            selected_speed_mask = ((-freq >= fp_max) & (-freq <= fs_max)) # negative frequencies
+            filter_line[selected_speed_mask] = np.cos(0.5 * np.pi *
+                                                            (freq[selected_speed_mask] + fp_max) / (fs_max - fp_max))
+        
+        # Stopband
+        filter_line[np.abs(freq) >= fs_max] = 0
+        filter_line[np.abs(freq) < fs_min] = 0
 
-    if return_half:
+        # Fill the filter matrix
+        fk_mask[i, :] *= filter_line
+
+    if fft_shift & return_half:
         fk_mask = fk_mask[:, :int(ns/2+1)]
-
+        fk_mask = np.fft.fftshift(fk_mask, axes=0)
+    elif fft_shift and not return_half:
+        fk_mask = np.fft.fftshift(fk_mask)
+    elif not fft_shift and return_half:
+        fk_mask = fk_mask[:, int(ns/2+1):]
+    
     return fk_mask
 
 def taper_constant(fk_mask, mask_type = 'tukey', widening_factor=.01, **kwargs):

test.py
@@ -30,7 +30,7 @@ import scipy.signal as sp
 
 # dw.plot.plot_tx(tx, t, x)
 
-# # load and plot original data:
+# load and plot original data:
 # fpath_orig = r'E:\testingData\OOI\DASData\OptaSense\North_C2\North-C2-HF-P1kHz-GL30m-Sp2m-FS500Hz_2021-11-02T220001Z.h5'
 # md = dw.data_handle.get_acquisition_parameters(fpath_orig, 'optasense')
 # selchan = settings['processing_settings']['selected_channels']
@@ -46,9 +46,9 @@ import scipy.signal as sp
 # dw.plot.plot_tx(txf_orig, t_orig, x_orig)
 
 
-tx_drive_letter = 'D'
+tx_data_path = r'C:\Users\ers334\Desktop\testingData'
 
-fpath = tx_drive_letter + r':\svalbard_r2\20220822_125007.h5'
+fpath = os.path.join(tx_data_path, r'svalbard_r2\20220822_120157.h5')
 fpn = os.path.split(fpath)
 print(fpn)
 data = io.load_chunk_h5(fpath)
@@ -63,13 +63,13 @@ x = np.arange(tx.shape[0])*dx
 #plt.imshow(20*np.log10(np.abs(tx)), )
 
 dw.plot.plot_tx(tx, t, x)
-fig = plt.gcf()
-plt.text(5, 39, 'Rehydrated Data', fontsize=16, color='w', horizontalalignment='center')
-plt.savefig(r'C:\Users\ers334\Documents\weeklyUpdates\2025_10_23\rehydrated_tx.png')
+# fig = plt.gcf()
+# plt.text(5, 39, 'Rehydrated Data', fontsize=16, color='w', horizontalalignment='center')
+# plt.savefig(r'C:\Users\ers334\Documents\weeklyUpdates\2025_10_23\rehydrated_tx.png')
 
 
 # load and plot original data:
-fpath_orig = tx_drive_letter + r':\testingData\Svalbard\data\125007.hdf5'
+fpath_orig = os.path.join(tx_data_path, r'Svalbard\data\120057.hdf5')
 md = dw.data_handle.get_acquisition_parameters(fpath_orig, 'asn')
 selchan = settings['processing_settings']['selected_channels']
 selchan[2] = 2
@@ -82,15 +82,23 @@ fk_filter_matrix = dw.dsp.fk_filter_design(tx_orig.shape, selchan, md['dx'], md[
 txf_orig = dw.dsp.fk_filter_filt(txf_orig, fk_filter_matrix)
 
 dw.plot.plot_tx(txf_orig, t_orig, x_orig)
-plt.text(5, 39, 'Original Data', fontsize=16, color='w', horizontalalignment='center')
-plt.savefig(r'C:\Users\ers334\Documents\weeklyUpdates\2025_10_23\original_tx.png')
+# plt.text(5, 39, 'Original Data', fontsize=16, color='w', horizontalalignment='center')
+# plt.savefig(r'C:\Users\ers334\Documents\weeklyUpdates\2025_10_23\original_tx.png')
 
 # move through the process step by step to see where amplitudes differences enter:
 fk_mask_orig = df.create_fk_mask(tx_orig.shape, md['dx'], md['fs'])
-fk_mask_orig_tapered = df.taper_mask(fk_mask_orig)
 
-tmp = dw.dsp.hybrid_ninf_filter_design(txf_orig.shape, selchan, dx, fs, cs_min=1400., cp_min=1450., cp_max=3400, cs_max=3500, fmin=5., fmax=fs/2-5, display_filter=True)
+# fk_mask_sparse = dw.dsp.hybrid_ninf_filter_design(tx_orig.shape, selchan, md['dx'], md['fs'], fmin=15, fmax=90, 
+#                             cs_min=1300., cp_min=1450., cp_max=4500, cs_max=6000, display_filter=True)
 
-ok=1
+# fk_mask_hyb = fk_mask_sparse.todense()
 
-# txfi = df.fk_interpolate()
+# txf = dw.dsp.fk_filter_sparsefilt(txf_orig, fk_mask_sparse, tapering=True)
+
+dw.plot.plot_tx(txf_orig, t_orig, x_orig)
+
+txfi, ti, xi = df.fk_interpolate(txf_orig, md['dx'], md['fs'], dx, fs, output_format='tx')
+
+scale_factor = txfi.max()/txf_orig.max()
+print(scale_factor)
+dw.plot.plot_tx(txfi, ti, xi)